45da2e7ca2da3ff4e7e9f10919a3593b176ffd95
unknown <olivier.metzinger@epitech.eu>
2019-01-10 02:13:39 +0100

    first

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articless():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'loremeozpfk zoepfk zopefk pozekf pozekf pozekf opzekf opzefkopezokpzefok zfeopkokpfzeopk',
+            'author':'Olivier mtzg',
+            'create_date':'09-01-2018'
+        },
+        {
+            'id': 1,
+            'title': 'Article 2',
+            'body': 'loremeozpfk zoepfk zopefk pozekf pozekf pozekf opzekf opzefkopezokpzefok zfeopkokpfzeopk',
+            'author':'kevin b',
+            'create_date':'09-01-2018'
+        },
+        {
+            'id': 1,
+            'title': 'Article 3',
+            'body': 'loremeozpfk zoepfk zopefk pozekf pozekf pozekf opzekf opzefkopezokpzefok zfeopkokpfzeopk',
+            'author':'Olivier mtzg',
+            'create_date':'09-01-2018'
+        }
+        ]
+    return articles
